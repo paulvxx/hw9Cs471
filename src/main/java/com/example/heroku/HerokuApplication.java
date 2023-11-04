2af7ceceeb07d18b4b33523b5efe6c2a474310ad
paulvxx <psv51037@gmail.com>
2023-11-03 18:57:33 -0600

    changed db 88

--- src/main/java/com/example/heroku/HerokuApplication.java
+++ src/main/java/com/example/heroku/HerokuApplication.java
@@ -75,13 +75,13 @@ public class HerokuApplication {
       String my_str = getRandomString();
       Statement stmt = connection.createStatement();
       stmt.executeUpdate("CREATE TABLE IF NOT EXISTS table_timestamp_and_random_string (tick timestamp, random_string varchar(30))");
-      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + my_str + "')");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      stmt.executeUpdate("INSERT INTO table_timestamp_and_random_string VALUES (now(), '" + getRandomString() + "')");
+      ResultSet rs = stmt.executeQuery("SELECT tick,random_string FROM ticks");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
         String st = getRandomString();
-        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + st);
+        output.add("Read from DB: " + rs.getTimestamp("tick") + " " + rs.getTimestamp("random_string"));
       }
 
       model.put("records", output);
